00766e90b2a5daa8fb32785dbffad89de170d923
yumatakei <8102246+yumatakei@users.noreply.github.com>
2023-02-19 23:30:54 +0900

    🐶 change github action test

---
+++ testdata/.php-cs-fixer.dist.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '',
+    ]);
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'braces' => true,
+        'blank_line_after_opening_tag' => true,
+        'linebreak_after_opening_tag' => true,
+        'declare_strict_types' => true,
+        '@PSR1' => true,
+        '@PSR2' => true,
+    ])
+    ->setFinder($finder);
