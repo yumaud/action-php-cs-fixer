a9522532cd4649edfb1b4bf56605a585bdd176af
yumaud <8102246+yumaud@users.noreply.github.com>
2023-02-25 23:24:19 +0900

    🐶 support github-check and github-pr-check

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,35 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 php-cs-fixer --version
 
-(php-cs-fixer fix "${INPUT_PATH}" \
-      --config="${INPUT_CONFIG}" \
-      --diff \
-      --dry-run || true) \
-  | python3 /usr/local/bin/parse.py \
-  | reviewdog -name="php-cs-fixer" \
-      -f=diff \
-      -reporter="${INPUT_REPORTER:-github-pr-review}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      "${INPUT_REVIEWDOG_FLAGS}" \
-  || exit 1
+if [ "${INPUT_REPORTER}" -eq "github-pr-review" ]; then
+	(php-cs-fixer fix "${INPUT_PATH}" \
+		--config="${INPUT_CONFIG}" \
+		--diff \
+		--dry-run || true) \
+	| python3 /usr/local/bin/parse.py \
+	| reviewdog -name="php-cs-fixer" \
+		-f=diff \
+	  -reporter=github-pr-review \
+		-filter-mode="${INPUT_FILTER_MODE}" \
+		-fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+		"${INPUT_REVIEWDOG_FLAGS}" \
+	|| exit 1
+elif [ "${INPUT_REPORTER}" -eq "github-check" ] || [ "${INPUT_REPORTER}" -eq "github-pr-check" ]; then
+	(php-cs-fixer fix "${INPUT_PATH}" \
+		--config="${INPUT_CONFIG}" \
+		--diff \
+		--dry-run
+		--format=checkstyle|| true) \
+	| python3 /usr/local/bin/parse.py \
+	| reviewdog -name="php-cs-fixer" \
+		-f=diff \
+		-reporter="${INPUT_REPORTER}" \
+		-filter-mode="${INPUT_FILTER_MODE}" \
+		-fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+		-level="${INPUT_LEVEL}" \
+		"${INPUT_REVIEWDOG_FLAGS}" \
+	|| exit 1
+else
+  echo "${INPUT_REPORTER} is not supporterd"
+  exit 1
+fi
