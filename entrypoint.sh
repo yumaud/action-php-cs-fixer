1fe2cbb1a9b92a9c398576a053b5c656951c3a5a
yumaud <8102246+yumaud@users.noreply.github.com>
2023-02-26 01:29:11 +0900

    🐶 return exit 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,7 @@ if [ "${INPUT_REPORTER}" = "github-pr-review" ]; then
 		-fail-on-error="${INPUT_FAIL_ON_ERROR}" \
 		"${INPUT_REVIEWDOG_FLAGS}" \
 	|| exit 1
+	exit 0
 elif [ "${INPUT_REPORTER}" = "github-check" ] || [ "${INPUT_REPORTER}" = "github-pr-check" ]; then
 	(php-cs-fixer fix "${INPUT_PATH}" \
 		--config="${INPUT_CONFIG}" \
@@ -39,6 +40,7 @@ elif [ "${INPUT_REPORTER}" = "github-check" ] || [ "${INPUT_REPORTER}" = "github
 		-level="${INPUT_LEVEL}" \
 		"${INPUT_REVIEWDOG_FLAGS}" \
 	|| exit 1
+	exit 0
 else
   echo "${INPUT_REPORTER} is not supporterd"
   exit 1
